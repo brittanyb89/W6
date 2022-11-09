db43c5d4220205219563dcece2e345c9a3fe1011
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-08 21:10:55 -0600

    Added class notes

--- pages/10-dynamic/api.service.js
+++ pages/10-dynamic/api.service.js
@@ -3,6 +3,7 @@ const URL = "https://api.github.com/users?per_page=5";
 // fetch, then (promise) is the same as async, await (syntactic sugar for 'then' keyword)
 export const getUsers = async () => {
   const response = await fetch(URL);
-  return (await response).json();
+  // if we mark the function as async, the return value will be wrapped in a promise
+  return response.json();
 };
 // syntactic sugar is syntax within a programming language that make things easier to read or express
