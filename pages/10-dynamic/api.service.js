073b107b0a1495f28470cd7ed7eb5d5ed9066899
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2022-11-08 21:09:09 -0600

    Added notes on async-await alternative

--- pages/10-dynamic/api.service.js
+++ pages/10-dynamic/api.service.js
@@ -1,1 +1,8 @@
 const URL = "https://api.github.com/users?per_page=5";
+
+// fetch, then (promise) is the same as async, await (syntactic sugar for 'then' keyword)
+export const getUsers = async () => {
+  const response = await fetch(URL);
+  return (await response).json();
+};
+// syntactic sugar is syntax within a programming language that make things easier to read or express
